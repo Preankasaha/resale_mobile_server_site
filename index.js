74b4a4be96294d4b32bc46b6cf247488c576fbe0
unknown <preansaha@gmail.com>
2022-11-24 16:10:41 +0600

    mongodb connection set

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-//middleware
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_USER);
 
+//mongodb connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.idvtewe.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req, res) => {
     res.send('resale mobile server is running')
