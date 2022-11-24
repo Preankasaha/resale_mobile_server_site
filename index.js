1821f640d80373ab95b4b4b1ba5be8cf014ed9dd
unknown <preansaha@gmail.com>
2022-11-24 15:59:07 +0600

    general setup

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-
+//middleware
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 
 
