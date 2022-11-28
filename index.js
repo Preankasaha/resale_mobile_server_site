6d559759cc2ef72e368a108617e2b95d9f25bc47
unknown <preansaha@gmail.com>
2022-11-28 09:08:03 +0600

    category based product api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -12,21 +13,65 @@ console.log(process.env.DB_USER);
 console.log(process.env.DB_PASS);
 
 //mongodb connection
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.idvtewe.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wkoqaez.mongodb.net/?retryWrites=true&w=majority`;
 
 console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
 async function run() {
     try {
-        const categoriesCollection = client.db('mobileResale').collection('categoriesName');
 
-        app.get('/category', async (req, res) => {
+        //db collection
+        const categoryCollection = client.db('mobileResale').collection('categoriesName');
+        const productCollection = client.db('mobileResale').collection('productsCollection');
+        const bookingCollection = client.db('mobileResale').collection('bookings')
+        const userCollection = client.db('mobileResale').collection('users')
+
+     
+        //get category api
+        app.get('/products-category', async (req, res) => {
             const query = {};
-            const categories = await categoriesCollection.find(query).toArray();
+            const categories = await categoryCollection.find(query).toArray();
             res.send(categories);
         })
+
+
+        // products by category api with category/:id route
+
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { categoryId: id }
+            const products = await productCollection.find(query).toArray();
+            res.send(products);
+        })
+
+         //all products
+        // app.get('/category', async (req, res) => {
+        //     const query = {};
+        //     const products = await productCollection.find(query).toArray();
+        //     res.send(products);
+        // })
 
     }
     finally {
